85b52433b085d574e2770745d976b1071ef517d7
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-11 12:43:08 +0000

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''minimum operations code'''
+def minOperations(n):
+    '''a method that calculates the fewest number of operations'''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,2 @@
+0x02 minimum oparations
+interview preparation task 
